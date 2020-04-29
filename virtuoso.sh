a31cf358ff29b1878212a5e3bae7ee2e9962a74f
Natanael Arndt <arndtn@gmail.com>
2020-04-29 11:48:10 +0200

    Update virtuoso.ini and virtuoso.sh to the state of current master (550b325f5e410cc660295c6a8ab5c05f74476fa3)

--- virtuoso.sh
+++ virtuoso.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+SETTINGS_DIR=/settings
+mkdir -p $SETTINGS_DIR
+
 cd /data
 
 mkdir -p dumps
@@ -11,32 +14,50 @@ fi
 chmod +x /clean-logs.sh
 mv /clean-logs.sh . 2>/dev/null
 
-if [ ! -f "/.config_set" ];
+original_port=`crudini --get virtuoso.ini HTTPServer ServerPort`
+# NOTE: prevents virtuoso to expose on port 8890 before we actually run
+#		the server
+crudini --set virtuoso.ini HTTPServer ServerPort 27015
+
+if [ ! -f "$SETTINGS_DIR/.config_set" ];
 then
   echo "Converting environment variables to ini file"
   printenv | grep -P "^VIRT_" | while read setting
   do
     section=`echo "$setting" | grep -o -P "^VIRT_[^_]+" | sed 's/^.\{5\}//g'`
-    key=`echo "$setting" | grep -o -P "_[^_]+=" | sed 's/[_=]//g'`
+    key=`echo "$setting" | sed -E 's/^VIRT_[^_]+_(.*)=.*$/\1/g'`
     value=`echo "$setting" | grep -o -P "=.*$" | sed 's/^=//g'`
     echo "Registering $section[$key] to be $value"
-    crudini --set virtuoso.ini $section $key $value
+    crudini --set virtuoso.ini $section $key "$value"
   done
-  touch /.config_set
+  echo "`date +%Y-%m%-dT%H:%M:%S%:z`" >  $SETTINGS_DIR/.config_set
   echo "Finished converting environment variables to ini file"
 fi
 
-if [ ! -f "/.dba_pwd_set" ];
+if [ ! -f ".backup_restored" -a -d "backups" -a ! -z "$BACKUP_PREFIX" ] ;
+then
+    echo "Start restoring a backup with prefix $BACKUP_PREFIX"
+    cd backups
+    virtuoso-t +restore-backup $BACKUP_PREFIX +configfile /data/virtuoso.ini
+    if [ $? -eq 0 ]; then
+        cd /data
+        echo "`date +%Y-%m-%dT%H:%M:%S%:z`" > .backup_restored
+    else
+        exit -1
+    fi
+fi
+
+if [ ! -f ".dba_pwd_set" ];
 then
   touch /sql-query.sql
   if [ "$DBA_PASSWORD" ]; then echo "user_set_password('dba', '$DBA_PASSWORD');" >> /sql-query.sql ; fi
   if [ "$SPARQL_UPDATE" = "true" ]; then echo "GRANT SPARQL_UPDATE to \"SPARQL\";" >> /sql-query.sql ; fi
   virtuoso-t +wait && isql-v -U dba -P dba < /dump_nquads_procedure.sql && isql-v -U dba -P dba < /sql-query.sql
-  kill $(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')
-  touch /.dba_pwd_set
+  kill "$(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')"
+  echo "`date +%Y-%m-%dT%H:%M:%S%:z`" >  .dba_pwd_set
 fi
 
-if [ ! -f "/.data_loaded" ] && [ -d "toLoad" ] ;
+if [ ! -f ".data_loaded" -a -d "toLoad" ] ;
 then
     echo "Start data loading from toLoad folder"
     pwd="dba"
@@ -51,8 +72,9 @@ then
     echo "$(cat /load_data.sql)"
     virtuoso-t +wait && isql-v -U dba -P "$pwd" < /load_data.sql
     kill $(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')
-    touch /.data_loaded
+    echo "`date +%Y-%m-%dT%H:%M:%S%:z`" > .data_loaded
 fi
 
-virtuoso-t +wait +foreground
+crudini --set virtuoso.ini HTTPServer ServerPort ${VIRT_HTTPServer_ServerPort:-$original_port}
 
+exec virtuoso-t +wait +foreground
